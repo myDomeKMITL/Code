78ffd0b0f32c8c67af4d05d2565bbfbf763e721d
Jeerakitti <title.jeerakitti@gmail.com>
2017-12-07 20:48:41 +0700

    bug fix

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'signin.php');
+	header('Location: '.$uri.'/signin.php');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
